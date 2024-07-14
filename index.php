c2efbfd85c49106e1458b9475fd6fd61d07605bc
pablolospe <yosoypxl@gmail.com>
2024-07-14 00:21:44 -0300

    favicon

--- index.php
+++ index.php
@@ -12,6 +12,8 @@ curl_close($ch);
 <head>
     <meta charset="UTF-8" />
     <title>Next Marvel Movie</title>
+    <link rel="icon" href="/assets/favicon.ico" type="image/x-icon">
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css" />
 </head>
 
